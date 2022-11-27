2db3615b35c1e415d96b7611cc1b48067d466d5b
kzok <yes.notyet@gmail.com>
2022-11-27 15:18:38 +0900

    add ExampleForm component

---
+++ src/components/example-form/example-form.tsx
@@ -0,0 +1,30 @@
+import { css } from "@emotion/react";
+import { Input } from "../input";
+import { Button } from "../button";
+
+const styles = {
+  container: css`
+    border: solid 1px #888;
+    padding: 16px;
+    display: flex;
+    flex-flow: column nowrap;
+    gap: 8px;
+  `,
+  row: css`
+    display: flex;
+    flex-flow: row nowrap;
+    gap: 8px;
+  `,
+} as const;
+
+export const ExampleForm: React.FC = () => {
+  return (
+    <div css={styles.container}>
+      <p>Chromatic で日本語を含むコンポーネントを描画できるかどうかのテスト</p>
+      <div css={styles.row}>
+        <Input placeholder="受付番号" defaultValue="" />
+        <Button>問い合わせ</Button>
+      </div>
+    </div>
+  );
+};
